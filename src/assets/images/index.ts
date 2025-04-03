45d97990626be351819fe9d91483c788777689aa
Emmanuel Ezema <emmanuelzema6@gmail.com>
2025-04-03 08:59:15 +0100

    Added more files and made minor changes"

---
+++ src/assets/images/index.ts
@@ -0,0 +1,26 @@
+import buildingImage1 from "./building1.jpg";
+import buildingImage2 from "./building2.jpg";
+import buildingImage3 from "./building3.jpg";
+import buildingImage4 from "./building4.jpg";
+import buildingImage5 from "./building5.jpg";
+import buildingImage6 from "./building6.jpg";
+import buildingImage7 from "./building7.jpg";
+import buildingInteriorImage1 from "./building-interior1.jpg";
+import buildingInteriorImage2 from "./building-interior2.jpg";
+import buildingInteriorImage3 from "./building-interior3.jpg";
+import buildingInteriorImage4 from "./building-interior4.jpg";
+
+
+export {
+    buildingImage1,
+    buildingImage2,
+    buildingImage3,
+    buildingImage4,
+    buildingImage5,
+    buildingImage6,
+    buildingImage7,
+    buildingInteriorImage1,
+    buildingInteriorImage2,
+    buildingInteriorImage3,
+    buildingInteriorImage4
+}
